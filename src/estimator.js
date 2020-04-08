839d5b3e5f41872c7604a99fa8c638c242651146
Qhristen <odeyxtian@gmail.com>
2020-04-08 13:00:10 +0100

    ch1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,26 @@
-const covid19ImpactEstimator = (data) => data;
+
+const covid19ImpactEstimator = (data) => {
+
+    const impact ={
+        currentlyInfected: data.reportedCases * 10,
+        infectionsByRequestedTime: currentlyInfected * 1024
+
+
+    }
+
+    const  severImpact ={
+        currentlyInfected: data.reportedCases * 50,
+        infectionsByRequestedTime: currentlyInfected * 1024
+
+    }
+
+
+  return {
+    data: data,
+    impact,
+    severImpact
+  };
+};
+
 
 export default covid19ImpactEstimator;
